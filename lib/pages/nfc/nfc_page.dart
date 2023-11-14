acd6325a1f110eccad0ade8e0f309ea91f0f0351
Kevin Kolaveri <kevin.kolaveri@edu.unito.it>
2023-11-14 18:16:37 +0100

    added heroTag for NFC

--- lib/pages/nfc/nfc_page.dart
+++ lib/pages/nfc/nfc_page.dart
@@ -9,49 +9,52 @@ class NfcPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Leggi Carta'),
-      ),
-      body: Flex(
-        direction: Axis.vertical,
-        children: [
-          Flexible(
-            flex: 2,
-            child: Container(
-              margin: const EdgeInsets.all(4),
-              constraints: const BoxConstraints.expand(),
-              decoration: BoxDecoration(border: Border.all()),
-              child: const SingleChildScrollView(
-                child: Text('non importante'),
+    return Hero(
+      tag: 'NFCPage',
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Leggi Carta'),
+        ),
+        body: Flex(
+          direction: Axis.vertical,
+          children: [
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: const EdgeInsets.all(4),
+                constraints: const BoxConstraints.expand(),
+                decoration: BoxDecoration(border: Border.all()),
+                child: const SingleChildScrollView(
+                  child: Text('non importante'),
+                ),
               ),
             ),
-          ),
-          Flexible(
-            flex: 3,
-            child: GridView.count(
-              padding: const EdgeInsets.all(4),
-              crossAxisCount: 2,
-              childAspectRatio: 4,
-              crossAxisSpacing: 4,
-              mainAxisSpacing: 4,
-              children: [
-                ElevatedButton(
-                    onPressed: _nfcController.readCard,
-                    child: const Text('Read Card')),
-                ElevatedButton(
-                    onPressed: _nfcController.testTicket,
-                    child: const Text('Test Ticket')),
-                ElevatedButton(
-                    onPressed: _nfcController.testSmartCard,
-                    child: const Text('Test SmartCard')),
-                ElevatedButton(
-                    onPressed: _nfcController.testDefault,
-                    child: const Text('Test')),
-              ],
+            Flexible(
+              flex: 3,
+              child: GridView.count(
+                padding: const EdgeInsets.all(4),
+                crossAxisCount: 2,
+                childAspectRatio: 4,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                children: [
+                  ElevatedButton(
+                      onPressed: _nfcController.readCard,
+                      child: const Text('Read Card')),
+                  ElevatedButton(
+                      onPressed: _nfcController.testTicket,
+                      child: const Text('Test Ticket')),
+                  ElevatedButton(
+                      onPressed: _nfcController.testSmartCard,
+                      child: const Text('Test SmartCard')),
+                  ElevatedButton(
+                      onPressed: _nfcController.testDefault,
+                      child: const Text('Test')),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
