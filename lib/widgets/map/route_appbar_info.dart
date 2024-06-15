137e3fd0622a3abcee10f2c8575d0a0a3aba3bfd
Kevin Kolaveri <kevin.kolaveri@edu.unito.it>
2024-06-15 23:15:24 +0200

    fix color for light theme and added animation

--- lib/widgets/map/route_appbar_info.dart
+++ lib/widgets/map/route_appbar_info.dart
@@ -3,94 +3,145 @@ import 'package:flutter_gtt/controllers/map/map_controller.dart';
 import 'package:flutter_gtt/models/gtt/route.dart';
 import 'package:get/get.dart';
 
-class RouteAppBarInfo extends StatelessWidget {
+class RouteAppBarInfo extends StatefulWidget {
   final MapPageController mapController;
-  final _empty = const SizedBox.shrink();
+
   const RouteAppBarInfo({super.key, required this.mapController});
+
+  @override
+  State<RouteAppBarInfo> createState() => _RouteAppBarInfoState();
+}
+
+class _RouteAppBarInfoState extends State<RouteAppBarInfo>
+    with SingleTickerProviderStateMixin {
+  final _empty = const SizedBox.shrink();
+  bool previousState = false;
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 300),
+  );
+  late final CurvedAnimation _curvedAnimation = CurvedAnimation(
+    parent: _animationController,
+    curve: Curves.easeInOut,
+  );
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleContainer() {
+    if (_curvedAnimation.status != AnimationStatus.completed) {
+      previousState = true;
+      _animationController.forward();
+    } else {
+      previousState = false;
+      _animationController.reverse();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      if (mapController.routePatterns.isEmpty) return _empty;
-      if (mapController.isPatternInitialized.isFalse) return _empty;
-      if (mapController.isAppBarExpanded.isFalse) return _empty;
+      bool canShow = widget.mapController.routePatterns.isNotEmpty &&
+          widget.mapController.isPatternInitialized.isTrue &&
+          widget.mapController.isAppBarExpanded.isTrue;
+      if (widget.mapController.routePatterns.isEmpty) return _empty;
+      if (widget.mapController.isPatternInitialized.isFalse) return _empty;
 
-      RouteWithDetails route = mapController.routes.values.first;
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              route.longName.toUpperCase(),
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+      if (canShow != previousState) {
+        _toggleContainer();
+      }
+      RouteWithDetails route = widget.mapController.routes.values.first;
+
+      return SizeTransition(
+        sizeFactor: _curvedAnimation,
+        child: SingleChildScrollView(
+          physics: const NeverScrollableScrollPhysics(),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                route.longName.toUpperCase(),
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            const Text('Direzione:'),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  text: mapController.firstStop[route.pattern.code]!.name,
-                  children: [
-                    const WidgetSpan(child: SizedBox(width: 10)),
-                    const WidgetSpan(
-                      alignment: PlaceholderAlignment.middle,
-                      child: Icon(
-                        Icons.arrow_right_alt,
+              const SizedBox(
+                height: 5,
+              ),
+              const Text('Direzione:'),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
+                    text: widget
+                        .mapController.firstStop[route.pattern.code]!.name,
+                    children: [
+                      const WidgetSpan(child: SizedBox(width: 10)),
+                      const WidgetSpan(
+                        alignment: PlaceholderAlignment.middle,
+                        child: Icon(
+                          Icons.arrow_right_alt,
+                        ),
                       ),
-                    ),
-                    const WidgetSpan(child: SizedBox(width: 10)),
-                    TextSpan(
-                      text: route.pattern.headsign,
-                    ),
-                  ]),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            DropdownMenu(
-              enableSearch: false,
-              inputDecorationTheme: const InputDecorationTheme(
-                constraints: BoxConstraints(
-                  maxHeight: 45,
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 0,
-                  horizontal: 10,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
+                      const WidgetSpan(child: SizedBox(width: 10)),
+                      TextSpan(
+                        text: route.pattern.headsign,
+                      ),
+                    ]),
               ),
-              width: Get.width * 0.9,
-              initialSelection: mapController.isPatternInitialized.isTrue
-                  ? mapController.routes.values.first.pattern
-                  : null,
-              onSelected: (pattern) => pattern == null
-                  ? null
-                  : mapController.setCurrentPattern(pattern),
-              dropdownMenuEntries: mapController.routePatterns
-                  .map((pattern) => DropdownMenuEntry(
-                        value: pattern,
-                        label:
-                            '${pattern.directionId}:${pattern.code.split(':').last} - ${pattern.headsign}',
-                      ))
-                  .toList(),
-            ),
-            const SizedBox(
-              height: 5,
-            )
-          ],
+              const SizedBox(
+                height: 15,
+              ),
+              DropdownMenu(
+                enableSearch: false,
+                inputDecorationTheme: const InputDecorationTheme(
+                  constraints: BoxConstraints(
+                    maxHeight: 45,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 10,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                width: Get.width * 0.9,
+                initialSelection:
+                    widget.mapController.isPatternInitialized.isTrue
+                        ? widget.mapController.routes.values.first.pattern
+                        : null,
+                onSelected: (pattern) => pattern == null
+                    ? null
+                    : widget.mapController.setCurrentPattern(pattern),
+                dropdownMenuEntries: widget.mapController.routePatterns
+                    .map((pattern) => DropdownMenuEntry(
+                          value: pattern,
+                          label:
+                              '${pattern.directionId}:${pattern.code.split(':').last} - ${pattern.headsign}',
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(
+                              route.pattern == pattern
+                                  ? Theme.of(context).focusColor
+                                  : null,
+                            ),
+                          ),
+                        ))
+                    .toList(),
+              ),
+              const SizedBox(
+                height: 10,
+              )
+            ],
+          ),
         ),
       );
     });
